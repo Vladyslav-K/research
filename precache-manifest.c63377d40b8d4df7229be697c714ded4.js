8ce98f1eecd87b390c9b56246a26d48afeb6b8b6
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-08-10 11:27:27 +0300

    Updates

---
+++ precache-manifest.c63377d40b8d4df7229be697c714ded4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25565799431232f0b9f69b6ce762b84e",
+    "url": "/research/index.html"
+  },
+  {
+    "revision": "e54b8b0e62c641947578",
+    "url": "/research/static/css/main.947c7cc3.chunk.css"
+  },
+  {
+    "revision": "d95ecdbed7d3c964e4ec",
+    "url": "/research/static/js/2.e73feadd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/research/static/js/2.e73feadd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54b8b0e62c641947578",
+    "url": "/research/static/js/main.4877e921.chunk.js"
+  },
+  {
+    "revision": "5ffb78fa12f9467ed561",
+    "url": "/research/static/js/runtime-main.f8506871.js"
+  }
+]);
